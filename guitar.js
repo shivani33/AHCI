a1a0ee4960bdf8b55b41f99449f8d9098abb4564
Cristiano M. Dalbem <ninja.dalbem@gmail.com>
2014-12-15 01:43:59 +0100

    Adds notesMatrix with countings of each notes
    As well as the names of each Note within the note circle.

---
+++ guitar.js
@@ -0,0 +1,14 @@
+var stringCoords = [10, 30, 50, 70, 90, 110 ];
+var nstrings = stringCoords.length;
+
+var fretCoords = [60, 160, 260, 360, 450, 545, 635, 725, 815, 900, 980, 1060, 1140, 1220, 1290, 1365, 1435, 1500, 1570, 1640, 1700, 1765, 1820 ];
+var nfrets = fretCoords.length;
+
+var notesMap = [
+	["E","F","Gb","G","Ab","A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B","C","Db","D","Eb","E"],
+	["B","C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"],
+	["G","Ab","A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B","C","Db","D","Eb","E","F","Gb","G"],
+	["D","Eb","E","F","Gb","G","Ab","A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B","C","Db","D"],
+	["A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab","A"],
+	["E","F","Gb","G","Ab","A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B","C","Db","D","Eb","E"]
+];
